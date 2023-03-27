eef0170e4c34d6a8425818be09d62443846714d6
LakshmiPosni <posni.l@northeastern.edu>
2023-03-26 20:34:43 -0400

    A7 Done

--- src/tuiter/explore/index.js
+++ src/tuiter/explore/index.js
@@ -1,39 +1,92 @@
+// import React from "react";
+// import PostSummaryList from "../post-summary-list";
+// import "./index.css";
+//
+//
+// const ExploreComponent = () => {
+//     return (
+//         <>
+//             <div className="row">
+//                 <div className="col-11 position-relative">
+//                     <input className="form-control rounded-pill ps-5" placeholder="Search Tuiter" />
+//                     <i className="fa fa-search wd-pos-search"></i>
+//                 </div>
+//                 <div className="col-1 d-flex align-items-center">
+//                     <i className="fa fa-cog wd-cog"></i>
+//                 </div>
+//             </div>
+//
+//             <ul className="nav nav-tabs mt-2 mb-2">
+//                 <li className="nav-item">
+//                     <a className="nav-link active" href="for-you.html">For you</a>
+//                 </li>
+//                 <li className="nav-item">
+//                     <a className="nav-link" href="trending.html">Trending</a>
+//                 </li>
+//                 <li className="nav-item">
+//                     <a className="nav-link" href="news.html">News</a>
+//                 </li>
+//                 <li className="nav-item">
+//                     <a className="nav-link" href="sports.html">Sports</a>
+//                 </li>
+//                 <li className="nav-item d-none d-md-block">
+//                     <a className="nav-link" href="entertainment.html">Entertainment</a>
+//                 </li>
+//             </ul>
+//
+//             <div className="position-relative">
+//                 <img src="../../images/starship.png" className="w-100" alt=""/>
+//                 <h1 class="wd-image-text">SpaceX Starship</h1>
+//             </div>
+//             <PostSummaryList />
+//         </>
+//     )
+// }
+//
+// export default ExploreComponent;
+
 import React from "react";
 import PostSummaryList from "../post-summary-list";
 import "./index.css";
+
 const ExploreComponent = () => {
-    return(
+    return (
         <>
             <div className="row">
                 <div className="col-11 position-relative">
-                    <input placeholder="Search Tuiter"
-                           className="form-control rounded-pill ps-5"/>
-                    <i className="bi bi-search position-absolute
-                       wd-nudge-up"></i>
+                    <input className="form-control rounded-pill ps-5" placeholder="Search Tuiter" />
+                    <i className="fa fa-search wd-pos-search"></i>
                 </div>
-                <div className="col-1">
-                    <i className="wd-bottom-4 text-primary float-end bi
-                       bi-gear-fill fs-2 position-relative"></i>
+                <div className="col-1 d-flex align-items-center">
+                    <i className="fa fa-cog wd-cog"></i>
                 </div>
             </div>
-            <ul className="nav nav-pills mb-2">
+
+            <ul className="nav nav-tabs mt-2 mb-2">
                 <li className="nav-item">
-                    <a className="nav-link active">For You</a>
+                    <a className="nav-link active" href="for-you.html">For you</a>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link">Trending</a>
+                    <a className="nav-link" href="trending.html">Trending</a>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link">News</a>
+                    <a className="nav-link" href="news.html">News</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="sports.html">Sports</a>
+                </li>
+                <li className="nav-item d-none d-md-block">
+                    <a className="nav-link" href="entertainment.html">Entertainment</a>
                 </li>
             </ul>
-            <div className="position-relative mb-2">
-                <img src="/images/starship.jpg" className="w-100"/>
-                <h1 className="position-absolute wd-nudge-up text-white">
-                    SpaceX Starship</h1>
+
+            <div className="position-relative">
+                <img src="../../images/starship.png" className="w-100" alt=""/>
+                <h1 class="wd-image-text">SpaceX Starship</h1>
             </div>
-            <PostSummaryList/>
+            <PostSummaryList />
         </>
-    );
-};
+    )
+}
+
 export default ExploreComponent;
--- src/tuiter/home/index.js
+++ src/tuiter/home/index.js
@@ -1,14 +1,31 @@
 import React from "react";
-import postObjects from './post-objects.json';
-import PostItem  from "./post-item";
+import TuitsList from "../tuits/tuits-list";
+import WhatsHappening from "./whats-happening";
 
-
-const HomePostsList = () => {
-    return(
-        <div>
-            {postObjects.map(post => <PostItem key={post._id} item={post}/>)}
-        </div>
+const HomeComponent = () => {
+    return (
+        <>
+            <h4>Home</h4>
+            <WhatsHappening/>
+            <TuitsList />
+        </>
     );
 };
+export default HomeComponent;
 
-export default HomePostsList;
+
+
+// import React from "react";
+// import postObjects from './post-objects.json';
+// import PostItem  from "./post-item";
+//
+//
+// const HomePostsList = () => {
+//     return(
+//         <div>
+//             {postObjects.map(post => <PostItem key={post._id} item={post}/>)}
+//         </div>
+//     );
+// };
+//
+// export default HomePostsList;
