b7305de2fa5258498d11b56ebd0d9b93f5bfc5f0
LakshmiPosni <posni.l@northeastern.edu>
2023-04-02 15:14:37 -0400

    tuit-service changes

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 // const TUITS_API = 'http://localhost:4000/api/tuits';
-const API_BASE = process.env.REACT_APP_API_BASE;
+const API_BASE = 'https://tuiter-node-server-app-5sd2.onrender.com';
 const TUITS_API = `${API_BASE}/tuits`;
 
 export const createTuit = async (tuit) => {
